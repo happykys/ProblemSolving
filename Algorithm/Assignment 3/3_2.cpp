b75d0a9095a10644bad1f7f3612a54c81dbfcc7d
happykys <dydtns9815@naver.com>
2019-10-13 17:32:55 +0900

    과제 3 Commit

--- Algorithm/Assignment 3/3_2.cpp
+++ Algorithm/Assignment 3/3_2.cpp
@@ -10,23 +10,26 @@ void swap(int &a, int &b)
 }
 void quickSort(int a[], int first, int last)
 {
-    int L = first + 1;
-    int R = last;
-    int pivot = a[first];
-    while(L < R) // L >= R이면 종료
+    if(first < last)
     {
-        while(a[L] < pivot) L++; // pivot보다 큰거나같은값 나올때까지
-        while(a[R] > pivot) R--; // pivot보다 작거나같은값 나올때까지
-        if(L <= R)
+        int L = first + 1;
+        int R = last;
+        int pivot = a[first];
+        while(L <= R)
         {
-            if(L < R) swap(a[L], a[R]); // stable
-            L++;
-            R--;
+            while(L <= last && a[L] < pivot) L++;
+            while(R >= first + 1 && a[R] >= pivot) R--;
+            if(L <= R)
+            {
+                swap(a[L], a[R]);
+                L++;
+                R--;
+            }
         }
+        swap(a[first], a[R]);
+        quickSort(a, first, R - 1);
+        quickSort(a, R + 1, last);
     }
-    swap(a[first], a[R]);
-    if(first < R) quickSort(a, first, R - 1);
-    if(last > L) quickSort(a, R + 1, last);
 }
 
 int main()
@@ -38,24 +41,25 @@ int main()
     for(int i = 0 ; i < n ; i++) cin >> arr[i];
 
     quickSort(arr, 0, n-1);
-
+    
     int cnt = 1;
     int maxCnt = 1;
-    int num;
+    int num = arr[0];
     for(int i = 1 ; i < n ; i++)
     {
         if(arr[i] == arr[i-1])
         {
             cnt++;
-            if(cnt >= maxCnt)
-            {
-                maxCnt = cnt;
-                num = arr[i];
-            }
         }
         else
         {
             cnt = 1;
+        }
+
+        if(cnt >= maxCnt)
+        {
+            maxCnt = cnt;
+            if(num < arr[i]) num = arr[i];
         }
     }
 
--- Algorithm/Assignment 3/3_3.cpp
+++ Algorithm/Assignment 3/3_3.cpp
@@ -2,8 +2,97 @@
 
 using namespace std;
 
+struct Person
+{
+    int arrive;
+    int spend;
+    int finish;
+};
+
+void rebuildHeap(Person a[], int root, int n)
+{
+    int i;
+    int x = a[root].finish;
+    int current = root;
+
+    while(2 * current + 1 < n)
+    {
+        int leftChild = 2 * current + 1;
+        int rightChild = leftChild + 1;
+        int smallChild;
+        if(rightChild < n && a[rightChild].finish <  a[leftChild].finish)
+        {
+            smallChild = rightChild;
+        }
+        else
+        {
+            smallChild = leftChild;
+        }
+
+        if(x > a[smallChild].finish)
+        {
+            a[current].finish = a[smallChild].finish;
+            current = smallChild;
+        }
+        else
+        {
+            break;
+        }
+    }
+    a[current].finish = x;
+}
+void toHeap(Person a[], int n)
+{
+    for(int root = n / 2 - 1 ; root >= 0 ; root--)
+    {
+        rebuildHeap(a, root, n);
+    }
+}
+
 int main()
 {
+    int n, k;
+    cin >> k;
+    cin >> n;
+    int arriveSum = 0;
+    double waiting = 0;
+    Person* Airport = new Person[n];
+    Person* Gate = new Person[k];
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> Airport[i].arrive >> Airport[i].spend;
+        Airport[i].arrive += arriveSum;
+        arriveSum = Airport[i].arrive;
+    }
 
+    for(int i = 0 ; i < k ; i++)
+    {
+        Gate[i] = Airport[i];
+        Gate[i].finish = Gate[i].arrive + Gate[i].spend;
+    }
+
+    toHeap(Gate, k);
+
+    for(int i = k ; i < n ; i++)
+    {
+        if(Gate[0].finish > Airport[i].arrive)
+        {
+            waiting += (Gate[0].finish - Airport[i].arrive);
+            Airport[i].finish = Gate[0].finish + Airport[i].spend;
+        }
+        else
+        {
+            Airport[i].finish = Airport[i].arrive + Airport[i].spend;
+        }
+        Gate[0] = Airport[i];
+        toHeap(Gate, k);
+    }
+
+    cout << fixed;
+    cout.precision(1);
+    cout << waiting / n;
+
+    delete[] Airport;
+    delete[] Gate;
     return 0;
 }
